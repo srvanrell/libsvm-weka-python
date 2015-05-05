9baf8441c33783dc8b2fd20b1f5f5c39919b1faa
srvanrell <srvanrell@gmail.com>
2015-05-05 18:08:11 -0300

    added installation directives

--- install_packages.py
+++ install_packages.py
@@ -3,24 +3,23 @@
 
 import weka.core.jvm as jvm
 import weka.core.packages as packages
-import sys
 
 jvm.start()
 
+# checking for installed packages
 installed_packages = packages.installed_packages()
 for item in installed_packages:
-    print item.name, item.url, "installed"
+    print item.name, item.url, "is installed\n"
 
-# Search for GridSearch and LibSVM
+# Search for GridSearch and LibSVM, just to check package's names
 all_packages = packages.all_packages()
 for item in all_packages:
     if (item.name == "gridSearch") or (item.name == "LibSVM"):
         print(item.name + " " + item.url)
 
-sys.stdout.flush()
-# packages.install_package("Name")
-# packages.uninstall_package("Name")
-
+# To install gridSearch and LibSVM
+packages.install_package("gridSearch", "1.0.8")
+packages.install_package("LibSVM")
 # To install MultiSearch
 packages.install_package("https://github.com/fracpete/multisearch-weka-package/releases/download/v2014.12.10/multisearch-2014.12.10.zip")
 
