2caa147fdf3e0617fa981b6a97986f9376f95c73
srvanrell <srvanrell@gmail.com>
2015-05-05 18:10:49 -0300

    cleaning code

--- install_packages.py
+++ install_packages.py
@@ -6,17 +6,6 @@ import weka.core.packages as packages
 
 jvm.start()
 
-# checking for installed packages
-installed_packages = packages.installed_packages()
-for item in installed_packages:
-    print item.name, item.url, "is installed\n"
-
-# Search for GridSearch and LibSVM, just to check package's names
-all_packages = packages.all_packages()
-for item in all_packages:
-    if (item.name == "gridSearch") or (item.name == "LibSVM"):
-        print(item.name + " " + item.url)
-
 # To install gridSearch and LibSVM
 packages.install_package("gridSearch", "1.0.8")
 packages.install_package("LibSVM")
