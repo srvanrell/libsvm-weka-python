6e8a283861da1467e0239d7e55c7e7cbe4413b56
srvanrell <srvanrell@gmail.com>
2016-03-30 11:46:41 -0300

    Cleaning old code

--- GridSearch-LibSVM.py
+++ GridSearch-LibSVM.py
@@ -3,7 +3,7 @@
 
 import weka.core.jvm as jvm
 from weka.core.converters import Loader
-from weka.classifiers import Classifier, Evaluation, SingleClassifierEnhancer
+from weka.classifiers import Classifier, Evaluation, SingleClassifierEnhancer, GridSearch
 import javabridge
 
 jvm.logger.setLevel(jvm.logging.WARNING)
@@ -16,28 +16,24 @@ trainData.class_is_last()
 
 # LibSVM with RBF kernel, cost C and gamma should be optimized
 classifier = Classifier(classname="weka.classifiers.functions.LibSVM",\
-                    options=["-S", "0", "-K", "2", "-Z", "-G", "0.0", "-C", "1.0",
-                             "-D", "3", "-R", "0.0", "-N", "0.5", "-M", "40.0",
-                             "-E", "0.001", "-P", "0.1", "-model", "~/", "-seed", "1"])
+                        options=["-S", "0", "-K", "2", "-Z", "-G", "0.0", "-C", "1.0",
+                                 "-D", "3", "-R", "0.0", "-N", "0.5", "-M", "40.0",
+                                 "-E", "0.001", "-P", "0.1", "-model", "~/", "-seed", "1"])
 
 # Logaritmic grid search on C and gamma, with cross validation (mandatory) on the training set
-grid = SingleClassifierEnhancer(
-    classname="weka.classifiers.meta.GridSearch",
-    options=[
-        "-E", "ACC",
-        "-x-property", "cost", "-x-min", "-3.0", "-x-max", "3.0", "-x-step", "1.0", "-x-base", "10.0",
-        "-x-expression", "pow(BASE,I)",
-        "-y-property", "gamma", "-y-min", "-3.0", "-y-max", "3.0", "-y-step", "1.0", "-y-base", "10.0",
-        "-y-expression", "pow(BASE,I)",
-        "-output-debug-info",
-        "-W", "weka.classifiers.trees.J48",  # dummy classifier added to avoid Nominal Class Capability Exception
-        "-sample-size", "100.0", "-num-slots", "1", "-S", "1"])
+grid = GridSearch(options=["-sample-size", "100.0", "-traversal", "ROW-WISE", "-num-slots", "1", "-S", "1",
+                           "-output-debug-info",
+                           #"-W", "weka.classifiers.trees.J48",  # dummy classifier added to avoid Nominal Class Capability Exception
+                           ])
+grid.evaluation = "ACC"
+grid.x = {"property": "cost", "min": -3.0, "max": 3.0, "step": 1.0, "base": 10.0, "expression": "pow(BASE,I)"}
+grid.y = {"property": "gamma", "min": -3.0, "max": 3.0, "step": 1.0, "base": 10.0, "expression": "pow(BASE,I)"}
 
 # LibSVM is added to grid configuration
 grid.classifier = classifier
 # Search for the best parameters and build a classifier with them
 grid.build_classifier(trainData)
-best = Classifier(jobject=javabridge.call(grid.jobject, "getBestClassifier", "()Lweka/classifiers/Classifier;"))
+best = grid.best
 best.build_classifier(trainData)
 
 print grid
--- install_packages.py
+++ install_packages.py
@@ -18,9 +18,9 @@ for item in all_packages:
         print(item.name + " " + item.url)
 
 # To install gridSearch and LibSVM
-packages.install_package("gridSearch", "1.0.8")
-packages.install_package("LibSVM")
+# packages.install_package("gridSearch", "1.0.8")
+# packages.install_package("LibSVM")
 # To install MultiSearch
-packages.install_package("https://github.com/fracpete/multisearch-weka-package/releases/download/v2014.12.10/multisearch-2014.12.10.zip")
+# packages.install_package("https://github.com/fracpete/multisearch-weka-package/releases/download/v2014.12.10/multisearch-2014.12.10.zip")
 
 jvm.stop()
