918f8047276f0144bb877b3ee5df5974c83ad8df
srvanrell <srvanrell@gmail.com>
2016-03-31 15:37:56 -0300

    Typo

--- loaderBug.py
+++ loaderBug.py
@@ -7,12 +7,11 @@ from weka.core.converters import Loader
 jvm.logger.setLevel(jvm.logging.INFO)
 jvm.start()
 
-# Each instance has nominal class and numeric attributes
 loader = Loader(classname="weka.core.converters.ArffLoader")
 trainData = loader.load_file('segment-challenge.arff')
 testData = loader.load_file('this-file-not-exist')  # silent fail
 
-print(trainData.num_instances)  # True answer
-print(testData.num_instances)   # False answer
+print(trainData.num_instances)  # True answer (1500)
+print(testData.num_instances)   # False answer (1500)
 
 jvm.stop()
