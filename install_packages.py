a1effed87a8e90483f1ab850c77aff7c827b7f48
srvanrell <srvanrell@gmail.com>
2016-04-11 13:44:48 -0300

    Add other options to install packages

--- install_packages.py
+++ install_packages.py
@@ -11,16 +11,19 @@ installed_packages = packages.installed_packages()
 for item in installed_packages:
     print item.name, item.url, "is installed\n"
 
-# Search for GridSearch and LibSVM, just to check package's names
-all_packages = packages.all_packages()
-for item in all_packages:
-    if (item.name == "gridSearch") or (item.name == "LibSVM"):
-        print(item.name + " " + item.url)
+# # Search for GridSearch and LibSVM, just to check package's names
+# all_packages = packages.all_packages()
+# for item in all_packages:
+#     if (item.name == "gridSearch") or (item.name == "LibSVM"):
+#         print(item.name + " " + item.url)
 
 # To install gridSearch and LibSVM
 # packages.install_package("gridSearch", "1.0.8")
 # packages.install_package("LibSVM")
 # To install MultiSearch
-# packages.install_package("https://github.com/fracpete/multisearch-weka-package/releases/download/v2014.12.10/multisearch-2014.12.10.zip")
+# packages.install_package("https://github.com/fracpete/multisearch-weka-package/releases/download/" +
+#                          "v2016.1.30/multisearch-2016.1.30.zip")
+# packages.install_package("/home/sebastian/Descargas/multisearch-2016.1.30.zip")
+# packages.uninstall_package("multisearch")
 
 jvm.stop()
