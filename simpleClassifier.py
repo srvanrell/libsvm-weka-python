ba0ebce8b6531fa1d9f728888c8d60d6ae6123dc
srvanrell <srvanrell@gmail.com>
2016-07-26 14:28:56 -0300

    Add python3 compatibility to simpleClassifier

--- simpleClassifier.py
+++ simpleClassifier.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
 
 import weka.core.jvm as jvm
 from weka.core.converters import Loader
@@ -21,22 +20,22 @@ classifier = Classifier(classname="weka.classifiers.trees.J48")
 # Search for the best parameters and build a classifier with them
 classifier.build_classifier(trainData)
 
-print "\n\n=========== Classifier information ================\n\n"
-print classifier.options
-print classifier
+print("\n\n=========== Classifier information ================\n\n")
+print(classifier.options)
+print(classifier)
 
-print "\n\n=========== Train results ================\n\n"
+print("\n\n=========== Train results ================\n\n")
 evaluation = Evaluation(trainData)
 evaluation.test_model(classifier, trainData)
-print classifier.to_commandline()
-print evaluation.matrix()
-print "Train recognition: %0.2f%%" % evaluation.percent_correct
+print(classifier.to_commandline())
+print(evaluation.matrix())
+print("Train recognition: %0.2f%%" % evaluation.percent_correct)
 
-print "\n\n=========== Test results ================\n\n"
+print("\n\n=========== Test results ================\n\n")
 evaluation = Evaluation(testData)
 evaluation.test_model(classifier, testData)
-print classifier.to_commandline()
-print evaluation.matrix()
-print "Test recognition: %0.2f%%" % evaluation.percent_correct
+print(classifier.to_commandline())
+print(evaluation.matrix())
+print("Test recognition: %0.2f%%" % evaluation.percent_correct)
 
 jvm.stop()
